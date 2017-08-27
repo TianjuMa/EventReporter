87c6a27accf65f8aeaafa2e596a610b61d64c7dd
jasonMatj <matianju@gmail.com>
2017-08-27 18:29:37 -0500

    Android app

---
+++ app/src/main/java/com/laioffer/eventreporter/EventAdapter.java
@@ -0,0 +1,76 @@
+package com.laioffer.eventreporter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class EventAdapter extends BaseAdapter {
+    Context context;
+    List<Event> eventData;
+
+    public EventAdapter(Context context) {
+        this.context = context;
+        eventData = DataService.getEventData();
+    }
+
+    @Override
+    public int getCount() {
+        return eventData.size();
+    }
+
+    @Override
+    public Event getItem(int position) {
+        return eventData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    /**
+     * Get a View that displays the data at the specified position in the data set. You can either
+     * create a View manually or inflate it from an XML layout file. When the View is inflated, the
+     * parent View (GridView, ListView...) will apply default layout parameters unless you use
+     * {@link LayoutInflater#inflate(int, ViewGroup, boolean)}
+     * to specify a root view and to prevent attachment to the root.
+     *
+     * @param position    The position of the item within the adapter's data set of the item whose view
+     *                    we want.
+     * @param convertView The old view to reuse, if possible. Note: You should check that this view
+     *                    is non-null and of an appropriate type before using. If it is not possible to convert
+     *                    this view to display the correct data, this method can create a new view.
+     *                    Heterogeneous lists can specify their number of view types, so that this View is
+     *                    always of the right type (see {@link #getViewTypeCount()} and
+     *                    {@link #getItemViewType(int)}).
+     * @param parent      The parent that this view will eventually be attached to
+     * @return A View corresponding to the data at the specified position.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.event_item,
+                    parent, false);
+        }
+
+        TextView eventTitle = (TextView) convertView.findViewById(
+                R.id.event_title);
+        TextView eventAddress = (TextView) convertView.findViewById(
+                R.id.event_address);
+        TextView eventDescription = (TextView) convertView.findViewById(
+                R.id.event_description);
+
+        Event r = eventData.get(position);
+        eventTitle.setText(r.getTitle());
+        eventAddress.setText(r.getAddress());
+        eventDescription.setText(r.getDescription());
+        return convertView;
+    }
+}
